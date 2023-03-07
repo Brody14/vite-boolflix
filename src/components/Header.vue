7770a19ccc2c9e67e25d99732656123dcbda22aa
Brody14 <stefania.spinas@gmail.com>
2023-03-07 14:24:20 +0100

    FEAT header

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<script >
+
+</script>
+
+<template>
+    <header class="main-header">
+        <figure class="header__logo">
+            <img src="/img/logo.png" alt="">
+        </figure>
+        <div class="search-bar">
+            <input class="search" type="text">
+            <button>Cerca</button>
+
+        </div>
+    </header>
+</template>
+
+<style lang="scss" >
+@use '../style/partials/variables' as *;
+
+.main-header {
+    background-color: $header-bg;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.header__logo {
+    width: 200px;
+}
+</style>
