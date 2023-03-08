ca9f9bf84a2901a5379ee484f84f4585bf2fa373
Brody14 <stefania.spinas@gmail.com>
2023-03-08 17:44:42 +0100

    EDIT header FEAT navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,46 @@
 <script >
 import store from '../store'
+import Navbar from './Navbar.vue'
 
 export default {
+    components: {
+        Navbar,
+    },
     data() {
         return {
             store,
+            show: false,
+            iconshow: true,
+
         }
     },
+    methods: {
+        toggleSearch() {
+            this.show = !this.show
+            this.iconshow = !this.iconshow
+        },
+    }
+
 }
 
 </script>
 
 <template>
-    <header class="main-header">
+    <header class="main-header" @click.self="toggleSearch">
         <figure class="header__logo">
             <img src="/img/logo.png" alt="">
         </figure>
+        <ul class="header-menu">
+            <Navbar />
+        </ul>
         <div class="search-bar">
-            <input @keyup.enter="$emit('onSearch')" class="search" type="text" v-model="store.search">
+            <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="search__icon" @click.stop="toggleSearch"
+                v-show="iconshow" />
+
+            <div>
+                <input v-show="show" @keyup.enter="$emit('onSearch')" class="search__input" type="text"
+                    v-model="store.search" placeholder="Cerca film o serie tv">
+            </div>
         </div>
     </header>
 </template>
@@ -29,11 +52,35 @@ export default {
     background-color: $header-bg;
     padding: 20px;
     display: flex;
-    align-items: center;
     justify-content: space-between;
+    align-items: center;
+    gap: 20px;
+
 }
 
 .header__logo {
     width: 150px;
+}
+
+.header-menu {
+    color: white;
+    display: flex;
+    gap: 10px;
+}
+
+.search-bar {
+    margin-left: auto;
+
+    .search__icon {
+        color: white;
+    }
+
+    .search__input {
+        background-color: unset;
+        border: 2px solid rgb(61, 61, 61);
+        padding: 10px;
+        border-radius: 15px;
+        color: grey;
+    }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<script >
+
+</script>
+
+<template>
+    <li class="navbar_item">
+        <a href="#">
+            Home
+        </a>
+    </li>
+    <li class="navbar_item">
+        <a href="#">
+            Serie TV
+        </a>
+    </li>
+    <li class="navbar_item">
+        <a href="#">
+            Film
+        </a>
+    </li>
+    <li class="navbar_item">
+        <a href="#">
+            Originali
+        </a>
+    </li>
+    <li class="navbar_item">
+        <a href="#">
+            Aggiunti di recente
+        </a>
+    </li>
+    <li class="navbar_item">
+        <a href="#">
+            La mia lista
+        </a>
+    </li>
+</template>
+
+<style lang="scss" scoped>
+.navbar_item:active {
+    font-weight: bold;
+}
+</style>
